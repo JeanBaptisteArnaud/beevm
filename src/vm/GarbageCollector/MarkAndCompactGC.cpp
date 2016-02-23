cb3ac4b0a95f059271636ec5e9f8150dbe8d6911
Javier Pimas <elpochodelagente@gmail.com>
2016-02-23 15:38:00 -0300

    start working on MarkAndCompact gc

---
+++ src/vm/GarbageCollector/MarkAndCompactGC.cpp
@@ -0,0 +1,145 @@
+
+#include "MarkAndCompactGC.h"
+#include "GarbageCollector.h"
+
+#include "../DataStructures/ObjectFormat.h"
+#include "../DataStructures/KnownObjects.h"
+#include "../DataStructures/Memory.h"
+
+using namespace Bee;
+
+void MarkAndCompactGC::addInterrupt()
+{
+	//self interrupt: 7
+}
+
+
+void MarkAndCompactGC::collect()
+{
+	this->loadSpaces();
+	this->initLocals();
+	this->unmarkRememberSet();
+	this->initAuxSpace();
+	this->unseeWellKnownObjects();
+	this->flushCodeCache();
+	this->clearPolymorphicMethodCache();
+	this->followAll();
+	this->setNewPositionsAndCompact();
+	this->updateOldSpace();
+	this->makeRescuedEphemeronsNonWeak();
+	this->updateNativeRescuedphemerons();
+	this->allocateArrays();
+	this->resetFrom();
+	this->decommitSlack();
+	this->addInterrupt();
+	this->saveSpaces();
+}
+
+void MarkAndCompactGC::unmarkRememberSet()
+{
+	tempArray.contents = rememberedSet.contents;
+	this->disableRememberSet();
+	for (int i = 1; i < tempArray.size()->_asNative(); i++)
+	{
+		oop_t* object = tempArray[i];
+		object->_beNotInRememberedSet();
+	}
+}
+
+void MarkAndCompactGC::initAuxSpace()
+{
+	auxSpace.loadFrom(oldSpace);
+	auxSpace.reset();
+}
+
+void MarkAndCompactGC::unseeWellKnownObjects()
+{
+	KnownObjects::nil->_beUnseenInLibrary();
+	KnownObjects::stTrue->_beUnseenInLibrary();
+	KnownObjects::stFalse->_beUnseenInLibrary();
+
+	(*vm.debugFrameMarker)->_beUnseenInLibrary();
+
+	this->unseeCharacters();
+	this->unseeSKernelMeta();
+	this->unseeLibraryObjects();
+}
+
+void MarkAndCompactGC::unseeCharacters()
+{
+	// characters.objectsDo(object _beUnseenInLibrary);
+}
+
+void MarkAndCompactGC::unseeSKernelMeta()
+{
+	// sLernelMeta.objectsDo(object _beUnseenInLibrary);
+}
+void MarkAndCompactGC::unseeLibraryObjects()
+{
+	// self librariesDo: [:sll | sll.objectsDo(object _beUnseenInLibrary)];
+}
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+void MarkAndCompactGC::()
+{
+}
+
+
---
+++ src/vm/GarbageCollector/MarkAndCompactGC.h
@@ -0,0 +1,57 @@
+/*
+ * MarkAndCompactGC.h
+ *
+ *  Created on: 18 dic. 2015
+ *      Author: Arnaud Jean-Baptiste
+ */
+
+#ifndef GARBAGECOLLECTOR_MARKANDCOMPACTGC_H_
+#define GARBAGECOLLECTOR_MARKANDCOMPACTGC_H_
+
+#include "GarbageCollector.h"
+#include "../DataStructures/GCSpace.h"
+
+namespace Bee {
+
+class MarkAndCompactGC: public GarbageCollector
+{
+
+public:
+	MarkAndCompactGC();
+	~MarkAndCompactGC();
+
+	void collect();
+
+	void addInterrupt();
+	void unmarkRememberSet();
+	void initAuxSpace(); 
+	void unseeWellKnownObjects(); 
+	void flushCodeCache();
+	void followAll();
+	void setNewPositionsAndCompact();
+	void updateOldSpace();
+	void updateNativeRescuedphemerons();
+	void allocateArrays();
+	void resetFrom();
+	void decommitSlack();
+	void disableRememberSet();
+	void unseeCharacters();
+	void unseeSKernelMeta();
+	void unseeLibraryObjects();
+	void followWellKnownObjects();
+	void followStack();
+	void followExtraRoots();
+	void followRescuedEphemerons();
+
+
+	ReferencedVMArray tempArray;
+	GCSpace characters;
+	GCSpace skernelMeta;
+	//SLLInfo library;
+
+
+};
+
+}
+
+#endif /* GARBAGECOLLECTOR_MARKANDCOMPACTGC_H_ */
